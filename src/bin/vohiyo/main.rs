76b08041f00a89f948668803df1bd585a94df8ad
museun <museun@outlook.com>
2023-04-23 23:19:19 -0400

    move to lib->binary split

--- src/bin/vohiyo/main.rs
+++ src/bin/vohiyo/main.rs
@@ -1,23 +1,7 @@
-mod db;
-mod helix;
-mod image;
-mod input;
-mod queue;
-mod repaint;
-mod resolver;
-mod runtime;
-mod state;
-mod twitch;
-mod util;
-mod views;
-mod widgets;
-
-mod app;
-
 #[tokio::main]
 async fn main() {
     simple_env_load::load_env_from([".dev.env", ".secrets.env"]);
-    let config = twitch::Config {
+    let config = vohiyo::twitch::Config {
         name: std::env::var("TWITCH_NAME").expect("'TWITCH_NAME' must be set'"),
         token: std::env::var("TWITCH_OAUTH").expect("'TWITCH_OAUTH' must be set'"),
     };
@@ -25,7 +9,7 @@ async fn main() {
     eframe::run_native(
         &format!("VoHiYo - {name}", name = config.name,),
         eframe::NativeOptions::default(),
-        Box::new(|cc| app::App::create(cc, config)),
+        Box::new(|cc| vohiyo::App::create(cc, config)),
     )
     .unwrap();
 }
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+mod db;
+mod helix;
+mod image;
+mod input;
+mod queue;
+mod repaint;
+mod resolver;
+mod runtime;
+mod state;
+mod util;
+mod views;
+mod widgets;
+
+pub mod twitch;
+
+mod app;
+pub use app::App;
